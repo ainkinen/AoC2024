737659a037ba9ff23f452b9fb1fe6d69ce72d4f0
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2024-12-09 11:00:22 +0200

    Add input parser to the template

--- src/day_x.rs
+++ src/day_x.rs
@@ -1,13 +1,21 @@
-use aoc_runner_derive::aoc;
+use aoc_runner_derive::{aoc, aoc_generator};
+
+struct Input {}
+
+#[aoc_generator(dayX)]
+fn parse_input(input: &str) -> Input {
+    let _ = input;
+    Input {}
+}
 
 #[aoc(dayx, part1)]
-fn part1(input: &str) -> i32 {
+fn part1(input: &Input) -> i32 {
     let _ = input;
     0
 }
 
 #[aoc(dayx, part2)]
-fn part2(input: &str) -> i32 {
+fn part2(input: &Input) -> i32 {
     let _ = input;
     0
 }
@@ -20,11 +28,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 0);
+        assert_eq!(part1(&parse_input(TEST_INPUT)), 0);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0);
+        assert_eq!(part2(&parse_input(TEST_INPUT)), 0);
     }
 }
